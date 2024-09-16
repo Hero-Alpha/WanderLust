fd3ad71a5dc43d6333bdcc96dc301361c32b9093
Hero-Alpha <yash2909.yk@gmail.com>
2024-09-16 18:15:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const { title } = require("process");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
+const app = express();
+const port = 8080;
+
+app.engine("ejs",ejsMate);
+app.set("view engine","ejs");
+app.set("views", path.join(__dirname,"/views"));
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(methodOverride("_method"));
+
+//DATABASE CONNECTION
+main()
+    .then(()=>{
+        console.log("Database connected");
+    })
+    .catch((err)=> console.log(err));
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+}
+
+app.listen(8080, ()=>{
+    console.log("Server listnening to port 8080");
+});
+
+//BASIC API
+app.get("/",(req, res)=>{
+    res.send("Server request accepted");
+});
+
+// ---------------------------------------------------------------------
+//INDEX ROUTE ("/listings")
+// to show all the listings in the database
+
+app.get("/listings", async(req,res)=>{
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs",{ allListings });
+});
+
+// --------------------------------------------------------------------------
+//CREATE ROUTE("/listings/new")
+//to create and save new data on site
+
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new"); 
+});
+
+
+app.post("/listings", async(req,res)=>{
+    let listing = req.body.listing;
+    let newListing = new Listing(listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+
+// --------------------------------------------------------------------------
+//EDIT  - UPDATE ROUTE("listings/:id/edit")
+app.get("/listings/:id/edit", async(req,res)=>{
+    let { id } = req.params;
+    console.log(id);
+    let listingData = await Listing.findById(id);
+    res.render("listings/edit",{ listingData });
+});
+
+app.put("/listings/:id", async(req,res)=>{
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect("/listings");
+});
+
+// --------------------------------------------------------------------------
+//DELETE ROUTE("/listings/:id")
+app.delete("/listings/:id", async(req,res)=>{
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+});
+
+
+
+// --------------------------------------------------------------------------
+//SHOW ROUTE ("/listings/:id")
+//to navigate to a particular listing in the data
+
+app.get("/listings/:id", async(req,res)=>{
+    let { id } = req.params;
+    console.log(id);
+    let listingData = await Listing.findById(id);
+    res.render("listings/show.ejs",{ listingData });
+});
---
+++ models/listing.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const linstingSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: String,
+    image: {
+        filename: { type: String, required: true },
+        url: { type: String, required: true }
+    },
+    // image: {
+    //     type: String,
+    //     default: "https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG90ZWx8ZW58MHx8MHx8fDA%3D",
+    //     set: (v) => v === "" ? "https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG90ZWx8ZW58MHx8MHx8fDA%3D" : v
+    // },
+    price: Number,
+    location: String,
+    country: String
+});
+
+const Listing = mongoose.model("Listing", linstingSchema);
+
+module.exports = Listing;
